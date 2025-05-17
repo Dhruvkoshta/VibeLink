5720790b6a06b91d0bdca28995240f6c3e8ce7f6
Dhruvkoshta <dhruvkoshta04@gmail.com>
2025-05-17 21:10:34 +0530

    update env

--- client/lib/socket.config.ts
+++ client/lib/socket.config.ts
@@ -5,7 +5,7 @@ import { getEnv } from "./utils"
 let socket:Socket
 export const getSocket = () => {
     if(!socket) {
-        socket = io(getEnv("BACKEND_URL"), {
+        socket = io(getEnv("NEXT_PUBLIC_BACKEND_URL"), {
             autoConnect: false,
         })
 
